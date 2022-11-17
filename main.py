748aed842ba9430eadbd25ec85c169872351f824
Samuel Brisio <samuelbrisio@outlook.com>
2022-11-17 19:57:26 -0300

    The program needs now a argument: config file path Config file: has the paths and file name

--- main.py
+++ main.py
@@ -2,58 +2,106 @@ import json
 import re
 import os
 import subprocess
+import sys
+import getopt
+import yaml
 
-#logPath = "/var/snort/"
-#rulePath = "/usr/local/etc/rules/"
-#newRulesPath = "/home/user/rule/"
-#sshAddrPath = "/usr/local/etc/snort/ssh/"
 
-# open log file
-jsonFile = open("example_file/alert_json.txt")
-IDS = {}
-
-# iterate over row in that directory
-for row in jsonFile:
-    data = json.loads(row)
-    id = re.split(":", data['rule'])[1]
-    IDS[id] = True
-
-# assign directory
-directory = 'example_file/rules'
-pathNewRules = "files/new_rules.rules"
-
-newRules = open(pathNewRules, "w")
- 
-# iterate over files in
-# that directory
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if not os.path.isfile(f):
-        continue
+def argumentsParsing(argv):
+    try:
+        opts, args = getopt.getopt(argv, 'hc:', 'config')
+    except getopt.GetoptError:
+        print('main.py -c <configfile>')
+        sys.exit(2)
     
-    File = open(f)
-    for row in File:
-        isNotRule = re.search("#", row)
-        
-        if isNotRule or len(row) == 0 or row == '\n':
+    parameters = []
+
+    if opts:
+        parameters = opts
+    elif args:
+        parameters = args
+    else:
+        print('main.py -c <configfile>')
+        sys.exit(2)
+    
+    return parameters 
+
+
+def yamlParsing(fileName):
+    try:
+        file = open(fileName, 'r')
+    except FileNotFoundError:
+        print("Arquivo não existe")
+        sys.exit(2)
+    
+    yamlFile = yaml.safe_load(file)
+    return yamlFile['paths'], yamlFile["file_names"]
+
+
+def main():
+    args = argumentsParsing(sys.argv[1:])
+    paths, names = yamlParsing(args[-1])
+
+    # open log file
+    jsonFile = open(paths['log'] + names['log'])
+    IDS = {}
+
+    # iterate over row in that directory
+    for row in jsonFile:
+        data = json.loads(row)
+        id = re.split(":", data['rule'])[1]
+        IDS[id] = True
+
+    # assign directory
+    directory = paths['rule']
+    pathNewRules = paths['new_rules'] + names['new_rules']
+
+    try:
+        newRules = open(pathNewRules, "w")
+    except FileNotFoundError:
+        print("Arquivo de log não encontrado")
+        sys.exit(2)
+    
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if not os.path.isfile(f):
             continue
+    
+        try:
+            File = open(f)
+        except FileNotFoundError:
+            print(f"Arquivo de regra não encontrado: {f}")
+            sys.exit(2)
 
-        Lista = re.findall("[;][\s]*[s][i][d][\s]*[:][\d]+[;]", row)
-        
-        # Se a lista terá que ter tamanho um, pois todas as linhas que não estão comentadas são regras
-        # E toda regra tem um id
-        if (len(Lista) != 1):
-            print(row)
-            assert True, "Tratar esse caso"
 
-        id = re.findall("[\d]+", Lista[0])[0]
+        for row in File:
+            isNotRule = re.search("#", row)
+            
+            if isNotRule or len(row) == 0 or row == '\n':
+                continue
 
-        if(IDS.get(id)):
-            newRules.write(row)
-        
+            Lista = re.findall("[;][\s]*[s][i][d][\s]*[:][\d]+[;]", row)
+            
+            # Se a lista terá que ter tamanho um, pois todas as linhas que não estão comentadas são regras
+            # E toda regra tem um id
+            if (len(Lista) != 1):
+                print(row)
+                assert True, "Tratar esse caso"
 
-sshFile = open("files/ssh_address_and_path.txt")
+            id = re.findall("[\d]+", Lista[0])[0]
 
-for sshAddr in sshFile:
-    subprocess.run(["scp", pathNewRules, sshAddr + "rules.rules"])
+            if(IDS.get(id)):
+                newRules.write(row)
+            
+    newRules.close()
+    sshFile = open("files/ssh_address_and_path.txt")
+
+    for sshAddr in sshFile:
+        subprocess.run(["scp", pathNewRules, sshAddr + "rules.rules"])
+
+
+if __name__ == "__main__":
+    main()
